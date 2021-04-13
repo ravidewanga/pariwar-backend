40ab4b325e8dbd7ae8919e479db648d19906097b
ravikant dewangan <ravidewangan13@gmail.com>
2021-04-13 22:43:31 +0530

    update google auth

--- app/controller/social.login.controller.js
+++ app/controller/social.login.controller.js
@@ -6,7 +6,7 @@ const client = new OAuth2Client('837613122270-hdk3k6aikmb4uhq92ub4og8tuui38ous.a
 
 async function verify(token) {
     const ticket = await client.verifyIdToken({
-        idToken:token,
+        idToken: token,
         audience: '837613122270-hdk3k6aikmb4uhq92ub4og8tuui38ous.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
         // Or, if multiple clients access the backend:
         //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
@@ -16,7 +16,7 @@ async function verify(token) {
     console.log(userid);
     // If request specified a G Suite domain:
     // const domain = payload['hd'];
-  }
+}
 
 // Retrieve all Tutorials from the database.
 exports.socialLogin = (req, res) => {
@@ -30,28 +30,32 @@ exports.socialLogin = (req, res) => {
 
     // Create a Social Data
     const { name, email, password, provider, provider_id, provider_pic, token, created_at, updated_at, active } = req.body.data;
-    
-    verify(token).catch(console.error);
 
-    const socialData = {
-        name: name,
-        email: email,
-        password: password,
-        provider: provider,
-        provider_id: provider_id,
-        provider_pic: provider_pic,
-        token: token
-        //published: req.body.published ? req.body.published : false
-    };
+    verify(token).then(() => {
+        console.log('1');
+        const socialData = {
+            name: name,
+            email: email,
+            password: password,
+            provider: provider,
+            provider_id: provider_id,
+            provider_pic: provider_pic,
+            token: token
+            //published: req.body.published ? req.body.published : false
+        };
 
-    // Save Social Data in the database
-    User.create(socialData).then(data => {
-        res.send(data);
-    })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while creating the Tutorial."
+        console.log('2');
+        // Save Social Data in the database
+        User.create(socialData).then(data => {
+            console.log('3');
+            res.send(data);
+        })
+            .catch(err => {
+                console.log('failed db insert');
+                res.status(500).send({
+                    message:
+                        err.message || "Some error occurred while creating the Tutorial."
+                });
             });
-        });
+    }).catch(console.error);
 };
