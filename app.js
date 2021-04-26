2e989773b14f43d6764b7df0ea292d44c08ca263
unknown <sinha.rahulsinha.sinha@gmail.com>
2021-04-26 22:21:14 +0530

    request data not shown

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require('./app/model/user.model');
-const Router = require('express').Router(); 
+const Router = express.Router(); 
 
 const app = express();
 app.use(Router);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
 mongoose.connect('mongodb+srv://admin:00rah0ul@pariwardb.k3kiw.mongodb.net/pariwar?retryWrites=true&w=majority', 
 {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -21,19 +25,18 @@ Router.get('/',(req,res,next) => {
 });
 
 
-Router.post('/',(req,res) => {
-    console.log('req-------------------------',req);
-    console.log('req body-------------------------',req.body);
+Router.post('/ravi',(req,res) => {
+    console.log('req-------------------------',req.body);
 
-    const user = new User({
-        _id : new mongoose.Types.ObjectId(),
-        name : req.body.name,
-        age : req.body.age
-    });
-    user.save().then(result => {
-        console.log(result);
-    })
-    .catch(err => console.log(err));
+    // const user = new User({
+    //     _id : new mongoose.Types.ObjectId(),
+    //     name : req.body.name,
+    //     age : req.body.age
+    // });
+    // user.save().then(result => {
+    //     console.log(result);
+    // })
+    // .catch(err => console.log(err));
 });
 
 const PORT = process.env.PORT || 8080;
