4bffdc1fec0b6e2f9393ce48cd5e1596af5bac47
samanthkumar33 <43128401+samanthkumar33@users.noreply.github.com>
2021-05-07 22:02:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(Routes);
-
+//port
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
