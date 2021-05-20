07da34309d0ec3b617979a00805f8579caed7c8e
unknown <sinha.rahulsinha.sinha@gmail.com>
2021-05-20 16:30:25 +0530

    form validation

--- app/controller/auth.controller.js
+++ app/controller/auth.controller.js
@@ -3,6 +3,25 @@ const User = require('../model/user.model');
 const jwt = require("jsonwebtoken");
 
 const login = (req,res) => {
+    let msg = [];
+    if(req.body.identity == '' && req.body.identity == undifined && req.body.identity === null){
+        msg.push('identity parameter missing');
+        // res.status(500).json({
+        //     code : 500,
+        //     msg : 'identity parameter missing'
+        // });
+    }
+    if(req.body.password == '' && req.body.password == undifined && req.body.password === null){
+        msg.push('password parameter missing');
+        
+    }
+    if(msg.length > 0){
+        res.status(500).json({
+                code : 500,
+                msg : msg
+        });
+    }
+
     const user = User.findOne(
         {   password : req.body.password,
             isactive:true,
